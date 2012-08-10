63de04f1f1a13863ee6c39e216df448830be1c8c
Joseph D. Olivarez <jdolivarez@gmail.com>
2012-08-09 19:33:12 -0500

    pagination complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,6 @@
 class PagesController < ApplicationController
 def index
- @links = Link.all
-
+@links = Link.paginate :page => params[:page], :per_page => 5, :order => 'updated_at DESC'
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @links }
